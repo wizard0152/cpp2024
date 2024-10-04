8e2268ade8328e4e70ba985d36227b340cecb0e3
Mambetov <aslan.mambetov.aslan@gmail.com>
2024-10-04 06:22:18 +0300

    Свежая домашка!

---
+++ sem1/Mambetov Aslan/MiniHomeWork2/task1.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include<iomanip>
+#include<cmath>
+#include<string>
+#include<vector>
+
+
+std::string convertNumber(long long num, int system) {
+    char digits[36]{ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', };
+    std::vector<int> rests;
+    std::string result;
+
+    while (num > 0) 
+    {
+        rests.push_back(num % system);
+        num /= system;
+    }
+
+    reverse(rests.begin(), rests.end());
+
+    for (auto rest : rests) 
+    {
+        result += digits[rest];
+    }
+    return result;
+}   
+
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    long long n;
+    long long sys;
+
+    std::cout << "Введите ваше число: ";
+    std::cin >> n;
+    std::cout << "\nВведите систему счисления в которую хотите перевести: ";
+    std::cin >> sys;
+    std::cout << "\nРезультат: " << convertNumber(n, sys) << "\n";
+}
---
+++ sem1/Mambetov Aslan/MiniHomeWork2/task2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include<iomanip>
+#include<cmath>
+#include<string>
+#include<vector>
+
+
+int Syracuse(long long num) {
+    while (num > 1)
+        num % 2 == 0 ? num /= 2 : num = (num * 3 + 1) / 2;
+    return num;
+}   
+
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    std::string something;
+
+    std::cout << "Итак, проверим ка гипотизу Сиракуз на практике. Попробуем проверить на натуральных числах до 100000, гипотеза опровергнится если программа зайдёт в бесконечный цикл.\n\nЕсли вы готовы, введите что угодно\n";
+    std::cin >> something;
+
+    for (int i = 1; i <= 100000; i++)
+    {
+        int s = Syracuse(i);
+        if(s == 1)
+            std::cout << "число " << i << " вернуло: " << Syracuse(i) << "   |   супер! Сработало\n";
+        else 
+        {
+            std::cout << "число " << i << " вернуло: " << Syracuse(i) << "Открытие!!! Мы смогли опровергнуть её! (такого конечно же не будет :)";
+            return 0;
+        } 
+    }
+    return 0;
+}
